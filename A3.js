8c42752334a538b23392375b63c66b44575924e8
Bogdan Lykhosherstov <bogdan.lykhosherstov@whitewhaleanalytics.com>
2017-11-21 13:50:24 -0700

    first commit

--- A3.js
+++ A3.js
@@ -1,20 +1,20 @@
 $(function(){
 	obesityPercent()});
-function obesityPercent(){	
-d3.selectAll("svg").remove();
-var width = 1200;
+function obesityPercent(){
+d3.selectAll(".first").remove();
+var width = 800;
 var height = 500;
 
 // D3 Projection
 var projection = d3.geo.albersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
-        
+
 // Define path generator
 var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-		
+
 // Define linear scale for output
 var color = d3.scale.linear()
 			  .range(["#77dd77","#fdfd96","#ffb347","#ff6961"]);
@@ -22,8 +22,9 @@ var color = d3.scale.linear()
 var legendText = ["Obesity: >29%", "Obesity: <=29%", "Obesity: <=26%", "Obesity: <=23%"];
 var graphName = ["Percentage of Obesity"];
 //Create SVG element and append map to the SVG
-var svg = d3.select("body")
+var svg = d3.select(".wrapper")
 			.append("svg")
+			.attr("class", "first")
 			.attr("width", width)
 			.attr("height", height);
 svg.append("text")
@@ -33,11 +34,11 @@ svg.append("text")
       	  .attr("y", 10)
       	  .attr("dy", ".70em")
       	  .text(function(d) { return d; });
-        
+
 // Append Div for tooltip to SVG
-var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
+var div = d3.select(".wrapper")
+		    .append("div")
+    		.attr("class", "tooltip")
     		.style("opacity", 0);
 
 // Load in my states data!
@@ -53,7 +54,7 @@ for (var i = 0; i < data.length; i++) {
 	// Grab State Name
 	var dataState = data[i].state;
 
-	// Grab data value 
+	// Grab data value
 	var dataValue = data[i].visited;
 
 	// Find the corresponding state inside the GeoJSON
@@ -63,14 +64,14 @@ for (var i = 0; i < data.length; i++) {
 		if (dataState == jsonState) {
 
 		// Copy the data value into the JSON
-		json.features[j].properties.visited = dataValue; 
+		json.features[j].properties.visited = dataValue;
 
 		// Stop looking through the JSON
 		break;
 		}
 	}
 }
-		
+
 // Bind the data to the SVG and create one path per GeoJSON feature
 svg.selectAll("path")
 	.data(json.features)
@@ -93,12 +94,12 @@ svg.selectAll("path")
 	}
 });
 
-		 
- 
-        
+
+
+
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend = d3.select("body").append("svg")
-      			.attr("class", "legend")
+var legend = d3.select(".wrapper").append("svg")
+      			.attr("class", "legend first")
      			.attr("width", 140)
     			.attr("height", 200)
    				.selectAll("g")
@@ -122,21 +123,21 @@ var legend = d3.select("body").append("svg")
 
 });
 }
-function displayExercise(){	
-d3.selectAll("svg").remove();
-var width = 1200;
+function displayExercise(){
+d3.selectAll(".first").remove();
+var width = 800;
 var height = 500;
 
 // D3 Projection
 var projection = d3.geo.albersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
-        
+
 // Define path generator
 var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-		
+
 // Define linear scale for output
 var color = d3.scale.linear()
 			  .range(["#77dd77","#fdfd96","#ffb347","#ff6961"]);
@@ -145,9 +146,10 @@ var legendText = ["Exercise: <70%", "Exerise: >=70%", "Exercise: >=75%", "Exerci
 var graphName = ["Percentage of population that exercises"];
 
 //Create SVG element and append map to the SVG
-var svg = d3.select("body")
+var svg = d3.select(".wrapper")
 			.append("svg")
 			.attr("width", width)
+			.attr("class", "first")
 			.attr("height", height);
 svg.append("text")
   		  .data(graphName)
@@ -156,11 +158,11 @@ svg.append("text")
       	  .attr("y", 10)
       	  .attr("dy", ".70em")
       	  .text(function(d) { return d; });
-        
+
 // Append Div for tooltip to SVG
-var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
+var div = d3.select(".wrapper")
+		    .append("div")
+    		.attr("class", "tooltip")
     		.style("opacity", 0);
 
 // Load in my states data!
@@ -176,7 +178,7 @@ for (var i = 0; i < data.length; i++) {
 	// Grab State Name
 	var dataState = data[i].state;
 
-	// Grab data value 
+	// Grab data value
 	var dataValue = data[i].exercise;
 
 	// Find the corresponding state inside the GeoJSON
@@ -186,14 +188,14 @@ for (var i = 0; i < data.length; i++) {
 		if (dataState == jsonState) {
 
 		// Copy the data value into the JSON
-		json.features[j].properties.exercise = dataValue; 
+		json.features[j].properties.exercise = dataValue;
 
 		// Stop looking through the JSON
 		break;
 		}
 	}
 }
-		
+
 // Bind the data to the SVG and create one path per GeoJSON feature
 svg.selectAll("path")
 	.data(json.features)
@@ -216,12 +218,12 @@ svg.selectAll("path")
 	}
 });
 
-		 
- 
-        
+
+
+
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend = d3.select("body").append("svg")
-      			.attr("class", "legend")
+var legend = d3.select(".wrapper").append("svg")
+				.attr("class", "legend first")
      			.attr("width", 140)
     			.attr("height", 200)
    				.selectAll("g")
@@ -245,21 +247,21 @@ var legend = d3.select("body").append("svg")
 
 });
 }
-function povertyRate(){	
-d3.selectAll("svg").remove();
-var width = 1200;
+function povertyRate(){
+d3.selectAll(".first").remove();
+var width = 800;
 var height = 500;
 
 // D3 Projection
 var projection = d3.geo.albersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
-        
+
 // Define path generator
 var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-		
+
 // Define linear scale for output
 var color = d3.scale.linear()
 			  .range(["#77dd77","#fdfd96","#ffb347","#ff6961"]);
@@ -268,9 +270,10 @@ var legendText = ["Poverty Rate: >=20", "Poverty Rate: >=17", "Poverty Rate: >=1
 var graphName = ["Poverty Rate"];
 
 //Create SVG element and append map to the SVG
-var svg = d3.select("body")
+var svg = d3.select(".wrapper")
 			.append("svg")
 			.attr("width", width)
+			.attr("class", "first")
 			.attr("height", height);
 svg.append("text")
   		  .data(graphName)
@@ -279,11 +282,11 @@ svg.append("text")
       	  .attr("y", 10)
       	  .attr("dy", ".70em")
       	  .text(function(d) { return d; });
-        
+
 // Append Div for tooltip to SVG
-var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
+var div = d3.select(".wrapper")
+		    .append("div")
+    		.attr("class", "tooltip")
     		.style("opacity", 0);
 
 // Load in my states data!
@@ -299,7 +302,7 @@ for (var i = 0; i < data.length; i++) {
 	// Grab State Name
 	var dataState = data[i].state;
 
-	// Grab data value 
+	// Grab data value
 	var dataValue = data[i].povRate;
 
 	// Find the corresponding state inside the GeoJSON
@@ -309,14 +312,14 @@ for (var i = 0; i < data.length; i++) {
 		if (dataState == jsonState) {
 
 		// Copy the data value into the JSON
-		json.features[j].properties.povRate = dataValue; 
+		json.features[j].properties.povRate = dataValue;
 
 		// Stop looking through the JSON
 		break;
 		}
 	}
 }
-		
+
 // Bind the data to the SVG and create one path per GeoJSON feature
 svg.selectAll("path")
 	.data(json.features)
@@ -339,12 +342,12 @@ svg.selectAll("path")
 	}
 });
 
-		 
- 
-        
+
+
+
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend = d3.select("body").append("svg")
-      			.attr("class", "legend")
+var legend = d3.select(".wrapper").append("svg")
+				.attr("class", "legend first")
      			.attr("width", 140)
     			.attr("height", 200)
    				.selectAll("g")
@@ -368,21 +371,21 @@ var legend = d3.select("body").append("svg")
 
 });
 }
-function goodWeight(){	
-d3.selectAll("svg").remove();
-var width = 1200;
+function goodWeight(){
+d3.selectAll(".first").remove();
+var width = 800;
 var height = 500;
 
 // D3 Projection
 var projection = d3.geo.albersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
-        
+
 // Define path generator
 var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-		
+
 // Define linear scale for output
 var color = d3.scale.linear()
 			  .range(["#77dd77","#fdfd96","#ffb347","#ff6961"]);
@@ -391,9 +394,10 @@ var legendText = ["Good Weight: < 34%", "Good Weight: >=34%", "Good Weight: >=37
 var graphName = ["Percentage of people with good weight"];
 
 //Create SVG element and append map to the SVG
-var svg = d3.select("body")
+var svg = d3.select(".wrapper")
 			.append("svg")
 			.attr("width", width)
+			.attr("class", "first")
 			.attr("height", height);
 svg.append("text")
   		  .data(graphName)
@@ -402,11 +406,11 @@ svg.append("text")
       	  .attr("y", 10)
       	  .attr("dy", ".70em")
       	  .text(function(d) { return d; });
-        
+
 // Append Div for tooltip to SVG
-var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
+var div = d3.select(".wrapper")
+		    .append("div")
+    		.attr("class", "tooltip")
     		.style("opacity", 0);
 
 // Load in my states data!
@@ -422,7 +426,7 @@ for (var i = 0; i < data.length; i++) {
 	// Grab State Name
 	var dataState = data[i].state;
 
-	// Grab data value 
+	// Grab data value
 	var dataValue = data[i].goodWeight;
 
 	// Find the corresponding state inside the GeoJSON
@@ -432,14 +436,14 @@ for (var i = 0; i < data.length; i++) {
 		if (dataState == jsonState) {
 
 		// Copy the data value into the JSON
-		json.features[j].properties.goodWeight = dataValue; 
+		json.features[j].properties.goodWeight = dataValue;
 
 		// Stop looking through the JSON
 		break;
 		}
 	}
 }
-		
+
 // Bind the data to the SVG and create one path per GeoJSON feature
 svg.selectAll("path")
 	.data(json.features)
@@ -462,12 +466,12 @@ svg.selectAll("path")
 	}
 });
 
-		 
- 
-        
+
+
+
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend = d3.select("body").append("svg")
-      			.attr("class", "legend")
+var legend = d3.select(".wrapper").append("svg")
+				.attr("class", "legend first")
      			.attr("width", 140)
     			.attr("height", 200)
    				.selectAll("g")
